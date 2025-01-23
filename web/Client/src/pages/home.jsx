3f1f26bd199ab253ad5481a141630aa215dab7fd
SanTechBoard <sanesunil07951369@gmail.com>
2025-01-23 16:20:44 +0530

    stupidity fix1

---
+++ web/Client/src/pages/home.jsx
@@ -0,0 +1,16 @@
+import { ThemeToggle } from '../components/themetoggle';
+import Dashboard from '../components/dashboard';
+
+function Home() {
+  return (
+    <>
+      <ThemeToggle />
+      <div className="max-w-4xl mx-auto px-4 mb-10">
+        <h1>Home test</h1>
+        <Dashboard />
+      </div>
+    </>
+  );
+}
+
+export default Home;

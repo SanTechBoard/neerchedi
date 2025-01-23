8126ac9e35fcb9ae3c5db0a1d737dfba6a96447c
SanTechBoard <sanesunil07951369@gmail.com>
2025-01-23 15:13:32 +0530

    nixpacks fix5

--- web/Client/vite.config.js
+++ web/Client/vite.config.js
@@ -5,11 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // Listen on all addresses
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: true
   },
   preview: {
-    host: true, // Listen on all addresses
+    host: '0.0.0.0',
     port: 3000,
+    strictPort: true
   }
 })
